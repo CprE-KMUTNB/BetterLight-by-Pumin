d7e64c49b53cfede65cf5932cc3aade3169368a1
Rookies111 <94309004+Rookies111@users.noreply.github.com>
2023-11-08 20:05:49 +0700

    Update script.js
    delete unnecessary function

--- public/script.js
+++ public/script.js
@@ -25,7 +25,6 @@ function onLoad() {
         status[3].innerHTML = `Color Temperature: ${default_status.color_temp}`
         item.getElementsByTagName("IMG")[0].src = `Assets/Light_${default_status.state}.png`
         document.getElementById("status").appendChild(item)
-        document.getElementById(`color${i+1}`).addEventListener("change", (event) => colorChange(event, item.id))
     }
 }
 
@@ -120,19 +119,4 @@ function changeMode(mode) {
     } else if (mode == 7) {
         webSocket.send('{"action":"4_double"}')
     }
-}
-
-function hexToRgb(hex) {
-    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-      return r + r + g + g + b + b;
-    });
-  
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-    } : null;
 }
